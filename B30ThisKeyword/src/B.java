658d38ce8b31c3a0f3820dd50210bcf4b1da8ae8
Sindhusree <sindhusindhu7384@gmail.com>
2022-11-07 17:31:09 +0530

    First commit

---
+++ B30ThisKeyword/src/B.java
@@ -0,0 +1,23 @@
+ class B {
+	 A4 obj;
+	 B(A4 obj){
+		 this.obj=obj;
+	 }
+	 void display() {
+		 System.out.println(obj.data);
+	 }
+	 }
+class A4{
+int data=10;
+A4(){
+	B b=new B(this);
+	b.display();
+}
+}
+class main{
+	public static void main(String[] args) {
+		A4 a=new A4();
+
+	}
+
+}
---
+++ B30ThisKeyword/src/com/og/thiskeyword/variables.java
@@ -0,0 +1,10 @@
+package com.og.thiskeyword;
+
+public class variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
