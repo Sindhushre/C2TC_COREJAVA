74467f51fe705ab81fa93d189f7d52a2a8d3c311
Sindhusree <sindhusindhu7384@gmail.com>
2022-11-07 17:48:54 +0530

    First commit

---
+++ FundamentalsCode/src/RelationalLogicalOperations.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class RelationalLogicalOperations {
+
+	public static void main(String[] args) {
+		 Random rand =new Random();
+		 int i =rand.nextInt(100);
+		 int j =rand.nextInt(100);
+		 System.out.println("i ="+ i);
+		 System.out.println("j ="+ j);
+		 System.out.println(i>j);
+		 System.out.println(i<j);
+
+	}
+
+}
