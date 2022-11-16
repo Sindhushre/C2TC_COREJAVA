4f3327e2090382b956f4b9e644a3d33ebb8876a5
Sindhusree <sindhusindhu7384@gmail.com>
2022-11-16 20:28:21 +0530

    First commit

---
+++ B30-Polymorphism/src/Adder.java
@@ -0,0 +1,25 @@
+
+ class Adder 
+ {
+	 static int add(int a, int b)
+	 {
+		 return a+b;
+	 }
+	 static int add(double a,double b)
+	 {
+		 return a+b;
+	 }
+ }
+ class TestOverloading1
+ {
+	 
+  public static void main(String[] args) {
+	  
+	  System.out.println(Adder.add(11,11, ));
+	   System.out.println(Adder.add(12,3,12,6));
+		
+
+	}
+ }
+ 
+
